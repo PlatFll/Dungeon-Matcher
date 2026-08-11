1d0467aa6c9f69f1600e0c2a2e16a92bdcd96466
PlatFll <103587619+PlatFll@users.noreply.github.com>
2026-08-11 03:16:10 +0300

    Add enemy action animation presenter

---
+++ Assets/_Game/Scripts/Combat/Enemies/Presentation/EnemyActionAnimationPresenter.cs
@@ -0,0 +1,133 @@
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(EnemyActor))]
+[RequireComponent(typeof(EnemyAutoAttack))]
+public sealed class EnemyActionAnimationPresenter : MonoBehaviour
+{
+    private const string VisualRootName = "VisualRoot";
+
+    private static readonly int AutoAttackTrigger =
+        Animator.StringToHash("AutoAttack");
+
+    private static readonly int AbilityTrigger =
+        Animator.StringToHash("Ability");
+
+    [SerializeField]
+    private Animator animator;
+
+    private EnemyActor enemyActor;
+    private EnemyAutoAttack enemyAutoAttack;
+
+    private void Awake()
+    {
+        ResolveReferences();
+    }
+
+    private void OnEnable()
+    {
+        ResolveReferences();
+
+        if (enemyAutoAttack != null)
+        {
+            enemyAutoAttack.AttackStarted +=
+                HandleAttackStarted;
+        }
+
+        if (enemyActor != null)
+        {
+            enemyActor.SpecialAbilityUsed +=
+                HandleSpecialAbilityUsed;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (enemyAutoAttack != null)
+        {
+            enemyAutoAttack.AttackStarted -=
+                HandleAttackStarted;
+        }
+
+        if (enemyActor != null)
+        {
+            enemyActor.SpecialAbilityUsed -=
+                HandleSpecialAbilityUsed;
+        }
+    }
+
+    private void ResolveReferences()
+    {
+        if (enemyActor == null)
+        {
+            enemyActor =
+                GetComponent<EnemyActor>();
+        }
+
+        if (enemyAutoAttack == null)
+        {
+            enemyAutoAttack =
+                GetComponent<EnemyAutoAttack>();
+        }
+
+        if (animator == null)
+        {
+            Transform visualRoot =
+                transform.Find(VisualRootName);
+
+            if (visualRoot != null)
+            {
+                animator =
+                    visualRoot.GetComponent<Animator>();
+            }
+        }
+    }
+
+    private void HandleAttackStarted(
+        EnemyAutoAttack attack)
+    {
+        PlayTrigger(AutoAttackTrigger);
+    }
+
+    private void HandleSpecialAbilityUsed(
+        EnemyActor enemy)
+    {
+        PlayTrigger(AbilityTrigger);
+    }
+
+    private void PlayTrigger(int triggerHash)
+    {
+        if (animator == null ||
+            !animator.isActiveAndEnabled ||
+            animator.runtimeAnimatorController == null)
+        {
+            return;
+        }
+
+        if (!HasTriggerParameter(triggerHash))
+        {
+            return;
+        }
+
+        animator.SetTrigger(triggerHash);
+    }
+
+    private bool HasTriggerParameter(
+        int triggerHash)
+    {
+        AnimatorControllerParameter[] parameters =
+            animator.parameters;
+
+        for (int i = 0; i < parameters.Length; i++)
+        {
+            if (parameters[i].nameHash == triggerHash &&
+                parameters[i].type ==
+                AnimatorControllerParameterType.Trigger)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
