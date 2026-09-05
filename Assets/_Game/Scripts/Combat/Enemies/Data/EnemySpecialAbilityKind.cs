d11f4c95ef584c878a310f1afda3c2ee9bf27d7d
PlatFll <103587619+PlatFll@users.noreply.github.com>
2026-09-06 00:37:42 +0300

    feat: register royal special ability kinds

--- Assets/_Game/Scripts/Combat/Enemies/Data/EnemySpecialAbilityKind.cs
+++ Assets/_Game/Scripts/Combat/Enemies/Data/EnemySpecialAbilityKind.cs
@@ -7,5 +7,7 @@ public enum EnemySpecialAbilityKind
     ShieldingAllies = 4,
     TownMarshal = 5,
     SiegeSergeant = 6,
-    KnightCaptain = 7
+    KnightCaptain = 7,
+    RoyalStandardBearer = 8,
+    CourtMage = 9
 }
